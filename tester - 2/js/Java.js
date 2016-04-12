a1e50255ce5d8267436daefea26f46b0e97a2da4
Plokhov <tetromit@mail.ru>
2016-04-12 14:08:58 +0300

    fuhwhf

---
+++ tester - 2/js/Java.js
@@ -0,0 +1,129 @@
+/*var a=prompt("  Кто пришел ?" , " " );
+if ( a==" Админ " )
+{
+var b=prompt("  Пароль ?" , " " );
+    if (b=="Черный Вазелин" )
+    { alert( "Добро пожаловать Мой Анальный Повелитель )")} else 
+            if (b==null) 
+            { alert ( "Вход Отменен" );}
+    else{ alert (" Введено не верное значение  ");}
+}
+else if (a==null) 
+{alert("Вход Отменен")} else {alert (" Введено не верное значение  ");}*/
+/*var a=5;
+switch (a)
+        { case 0: 
+            case 4:
+            case 5: alert("Питон");
+            case 7: alert(" Питуний");}*/
+/*var a=prompt("Сколько ворон седит на Ветке ? ","");
+a=parseInt(a);
+switch (a)
+{
+    case 0:
+            alert(" На ветке Нет Ворон ");
+            break;
+    case 1:
+            alert(" На ветке Сидит Одна Ворона ");
+            break;
+    case(2):
+            alert(" На ветке Сидит Две Вороны");
+            break;
+    case 3:
+            alert(" На ветке Сидит Три вороны ");
+            break;
+    case 4:
+            alert(" На ветке Сидит Четыре Вороны ");
+            break;
+    case 5:
+            alert(" На ветке Сидит Пять Ворон ");
+            break;
+    default:
+            alert(" Дохрена Ворона )) ");
+}
+
+for (var i=0;i<10;i++)
+{
+     continue;
+document.write(i+"<br>");
+}*/
+
+
+/*for (var i=0;i<10;i++)
+{
+    for ( var j=0; j<20 ;j++ )
+    {
+    document.write(j);
+    }
+ if (i==4) continue;
+document.write(i+"<br>");
+ if ( i==8 ) break;
+    document.write(i+"<br>");
+}
+
+
+var black="#000000";
+var white="#ffffff";
+var n=10;
+var a=1;
+var color=black;
+
+document.write("<table>");
+
+for (var tr=1;tr<=n;tr++)
+{
+    document.write("<tr>");
+    for (var td=1;td<=n;td++)
+    {
+    
+        document.write('<td>')
+       
+        document.write(tr*td)
+        
+       
+        
+        document.write("</td>")
+    
+     
+        
+        
+    }
+    document.write("</tr>");
+}
+
+document.write("</table>");
+
+var a=1;
+do 
+{
+a=a+1;
+    if (a==20) continue;
+    document.write(a+"<br>");
+    //if (a==31) break;
+}
+while (a<50);
+
+do
+{
+var name = prompt("Введите ваше имя", "");
+	
+			if ( !name )
+				name = "незнакомец";
+			
+			
+			var s = "<h1>Привет, "+name+"!</h1>";
+			document.write(s);
+}
+while (!name)*/
+
+
+try
+{
+alert(" Проба 1");
+    sncscbnnb;
+alert(" Проба 1");
+}
+catch(e)
+{
+alert("ошибка");
+}
